9867e8a856d0e6fc028bade94e3200631d842cfd
DziedMaroz <ilya.arkhanhelsky@gmail.com>
2011-10-16 23:40:32 +0300

    Initial commit 	new file:   README 	new file:   background.png 	new file:   layouts/dwindle.png 	new file:   layouts/dwindlew.png 	new file:   layouts/fairh.png 	new file:   layouts/fairhw.png 	new file:   layouts/fairv.png 	new file:   layouts/fairvw.png 	new file:   layouts/floating.png 	new file:   layouts/floatingw.png 	new file:   layouts/fullscreen.png 	new file:   layouts/fullscreenw.png 	new file:   layouts/magnifier.png 	new file:   layouts/magnifierw.png 	new file:   layouts/max.png 	new file:   layouts/maxw.png 	new file:   layouts/spiral.png 	new file:   layouts/spiralw.png 	new file:   layouts/tile.png 	new file:   layouts/tilebottom.png 	new file:   layouts/tilebottomw.png 	new file:   layouts/tileleft.png 	new file:   layouts/tileleftw.png 	new file:   layouts/tiletop.png 	new file:   layouts/tiletopw.png 	new file:   layouts/tilew.png 	new file:   submenu.png 	new file:   taglist/squarefw.png 	new file:   taglist/squarew.png 	new file:   tasklist/floating.png 	new file:   tasklist/floatingw.png 	new file:   theme.lua 	new file:   titlebar/close_focus.png 	new file:   titlebar/close_normal.png 	new file:   titlebar/floating_focus_active.png 	new file:   titlebar/floating_focus_inactive.png 	new file:   titlebar/floating_normal_active.png 	new file:   titlebar/floating_normal_inactive.png 	new file:   titlebar/maximized_focus_active.png 	new file:   titlebar/maximized_focus_inactive.png 	new file:   titlebar/maximized_normal_active.png 	new file:   titlebar/maximized_normal_inactive.png 	new file:   titlebar/ontop_focus_active.png 	new file:   titlebar/ontop_focus_inactive.png 	new file:   titlebar/ontop_normal_active.png 	new file:   titlebar/ontop_normal_inactive.png 	new file:   titlebar/sticky_focus_active.png 	new file:   titlebar/sticky_focus_inactive.png 	new file:   titlebar/sticky_normal_active.png 	new file:   titlebar/sticky_normal_inactive.png

---
+++ theme.lua
@@ -0,0 +1,96 @@
+---------------------------
+-- DM's awesome theme --
+---------------------------
+
+theme = {}
+
+theme.font          = "sans 8"
+
+theme.bg_normal     = "#2e3436"
+theme.bg_focus      = "#D3D7CF"
+theme.bg_urgent     = "#000000"
+theme.bg_minimize   = "#444444"
+
+theme.fg_normal     = "#D3D7CF"
+theme.fg_focus      = "#2e3436"
+theme.fg_urgent     = "#edd400"
+theme.fg_minimize   = "#ffffff"
+
+theme.border_width  = "1"
+theme.border_normal = "#2e3436"
+theme.border_focus  = "#535d6c"
+theme.border_marked = "#91231c"
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- Display the taglist squares
+theme.taglist_squares_sel   = "/home/lucian/.config/awesome/themes/default/taglist/squarefw.png"
+theme.taglist_squares_unsel = "/home/lucian/.config/awesome/themes/default/taglist/squarew.png"
+
+theme.tasklist_floating_icon = "/home/lucian/.config/awesome/themes/default/tasklist/floatingw.png"
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = "/home/lucian/.config/awesome/themes/default/submenu.png"
+theme.menu_height = "15"
+theme.menu_width  = "100"
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- Define the image to load
+theme.titlebar_close_button_normal = "/home/lucian/.config/awesome/themes/default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = "/home/lucian/.config/awesome/themes/default/titlebar/close_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = "/home/lucian/.config/awesome/themes/default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = "/home/lucian/.config/awesome/themes/default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = "/home/lucian/.config/awesome/themes/default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = "/home/lucian/.config/awesome/themes/default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = "/home/lucian/.config/awesome/themes/default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = "/home/lucian/.config/awesome/themes/default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = "/home/lucian/.config/awesome/themes/default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = "/home/lucian/.config/awesome/themes/default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = "/home/lucian/.config/awesome/themes/default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = "/home/lucian/.config/awesome/themes/default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = "/home/lucian/.config/awesome/themes/default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = "/home/lucian/.config/awesome/themes/default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = "/home/lucian/.config/awesome/themes/default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = "/home/lucian/.config/awesome/themes/default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = "/home/lucian/.config/awesome/themes/default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = "/home/lucian/.config/awesome/themes/default/titlebar/maximized_focus_active.png"
+
+-- You can use your own command to set your wallpaper
+theme.wallpaper_cmd = { "awsetbg /home/lucian/.config/awesome/themes/default/background.png" }
+
+-- You can use your own layout icons like this:
+theme.layout_fairh = "/home/lucian/.config/awesome/themes/default/layouts/fairhw.png"
+theme.layout_fairv = "/home/lucian/.config/awesome/themes/default/layouts/fairvw.png"
+theme.layout_floating  = "/home/lucian/.config/awesome/themes/default/layouts/floatingw.png"
+theme.layout_magnifier = "/home/lucian/.config/awesome/themes/default/layouts/magnifierw.png"
+theme.layout_max = "/home/lucian/.config/awesome/themes/default/layouts/maxw.png"
+theme.layout_fullscreen = "/home/lucian/.config/awesome/themes/default/layouts/fullscreenw.png"
+theme.layout_tilebottom = "/home/lucian/.config/awesome/themes/default/layouts/tilebottomw.png"
+theme.layout_tileleft   = "/home/lucian/.config/awesome/themes/default/layouts/tileleftw.png"
+theme.layout_tile = "/home/lucian/.config/awesome/themes/default/layouts/tilew.png"
+theme.layout_tiletop = "/home/lucian/.config/awesome/themes/default/layouts/tiletopw.png"
+theme.layout_spiral  = "/home/lucian/.config/awesome/themes/default/layouts/spiralw.png"
+theme.layout_dwindle = "/home/lucian/.config/awesome/themes/default/layouts/dwindlew.png"
+
+theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
+
+return theme
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=80
